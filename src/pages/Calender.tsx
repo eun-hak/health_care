97e9ec002c77c1efb94ff5f9e661ce63e90ef069
조기범 <akfmzh1207@naver.com>
2023-04-25 13:45:16 +0900

    주석처리

--- src/pages/Calender.tsx
+++ src/pages/Calender.tsx
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Nav from "../components/Nav";
+import Signiture from "../components/Signiture";
+import { Container, Wrapper, Container2 } from "../components/Layout";
+// import Calendar2 from "../components/Calender/Calender2";
 
 const Calender = () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Container2>{/* <Calendar2></Calendar2> */}</Container2>
+        <Wrapper>
+          <Signiture />
+          <Nav />
+        </Wrapper>
+      </Container>
+    </>
+  );
 };
 
 export default Calender;
+
+const Container3 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 1200px;
+  height: 100%;
+  border: 1px solid black;
+  margin: 80px;
+  background: white;
+  border-radius: 16px;
+`;
